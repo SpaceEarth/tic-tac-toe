150d956f73e7722c206876ea44523d4323dad7b4
alecsey <topsecret@gamil.com>
2019-09-23 17:09:37 +0300

    stage version

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,14 +1,20 @@
+const PLAYER_SYMBOLS = ['x', 'o'];
+
 class TicTacToe {
     constructor() {
-
+        this.playMatrix = new Array(3).fill(0).map(() => new Array(3).fill(null));
+        this.currentPlayerNumber = 0;
     }
 
     getCurrentPlayerSymbol() {
-
+        return PLAYER_SYMBOLS[this.currentPlayerNumber];
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        if (this.playMatrix[rowIndex][columnIndex] === null) {
+            this.playMatrix[rowIndex][columnIndex] = PLAYER_SYMBOLS[this.currentPlayerNumber];
+            this.currentPlayerNumber = (this.currentPlayerNumber + 1) % PLAYER_SYMBOLS.length;
+        }
     }
 
     isFinished() {
@@ -16,19 +22,25 @@ class TicTacToe {
     }
 
     getWinner() {
-
+        // this.playMatrix.some((row) => {
+        //     return row.join('').match(/.{3,}/g)
+        // })
     }
 
     noMoreTurns() {
-
+        return this.playMatrix.every((el) => {
+            return el.every((el) => {
+                return el !== null;
+            });
+        });
     }
 
     isDraw() {
-
+        
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.playMatrix[rowIndex][colIndex];
     }
 }
 
